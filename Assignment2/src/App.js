763536229261dd5e46d101a65429436a93a3b5fd
sandhyabutar <india.mymail@gmail.com>
2022-02-09 12:57:14 +0530

    create react app

---
+++ Assignment2/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import header from "./MyComponents/Header"
+
+function App() {
+  return (
+  <>
+    <h1>Butargirl</h1>
+    <h3>sandhya</h3>
+    <header/>
+  </>
+  
+  );
+}
+
+export default App;
---
+++ Assignment2/src/san.js
@@ -0,0 +1,10 @@
+const Vinay = ()=> <h1>{"Vinay"}</h1>
+
+function Abc(){
+    let  a = <>
+        <h1>ABC</h1>
+        <Vinay/>
+    </>;
+    return a;
+}
+export default Abc;
